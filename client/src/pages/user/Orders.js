fd5f822525857b63b904e3283d85b9a6afeab8f9
miuna-9890 <10mithuna@gmail.com>
2026-03-23 10:48:40 +0800

    Update Orders.js

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -25,7 +25,7 @@ const Orders = () => {
 
   useEffect(() => {
     if (auth?.token) getOrders();
-  }, [auth?.token, auth?.user]);
+  }, [auth?.token, auth?.user]); //Varatharaju Mithuna, A0281223N
   return (
     <Layout title={"Your Orders"}>
       <div className="container-flui p-3 m-3 dashboard">

e46fd44aa901727edc1829e8b9594059773d4017
miuna-9890 <10mithuna@gmail.com>
2026-03-23 09:17:00 +0800

    Update auth-Order-endpoint.test.js

--- integration-tests/auth-Order-endpoint.test.js
+++ integration-tests/auth-Order-endpoint.test.js
@@ -27,9 +27,9 @@ app.use('/api/v1/auth', authRoutes);
 let mongoServer;
 
 beforeAll(async () => {
-    mongoServer = await MongoMemoryServer.create();
-    const uri = mongoServer.getUri();
+    const uri = mongoServer.getUri("orders-test-db"); // unique name
 
+    await mongoose.disconnect(); // ensures clean connection
     await mongoose.connect(uri);
 });
 

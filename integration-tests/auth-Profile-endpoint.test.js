72750db25b054cbafcd58e98be59996eedce5e2c
miuna-9890 <10mithuna@gmail.com>
2026-03-23 09:17:33 +0800

    Update auth-Profile-endpoint.test.js

--- integration-tests/auth-Profile-endpoint.test.js
+++ integration-tests/auth-Profile-endpoint.test.js
@@ -23,9 +23,9 @@ app.use('/api/v1/auth', authRoutes);
 let mongoServer;
 
 beforeAll(async () => {
-    mongoServer = await MongoMemoryServer.create();
-    const uri = mongoServer.getUri();
+    const uri = mongoServer.getUri("profile-test-db"); // unique name
 
+    await mongoose.disconnect(); // ensures clean connection
     await mongoose.connect(uri);
 });
 

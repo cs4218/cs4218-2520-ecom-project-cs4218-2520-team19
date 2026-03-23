06b91615a813b61bed6a05fa6f0c9dbc9d812fca
miuna-9890 <10mithuna@gmail.com>
2026-03-23 09:43:37 +0800

    Update updateOrderStatusToGetOrders.endpoint.test.js

--- integration-tests/updateOrderStatusToGetOrders.endpoint.test.js
+++ integration-tests/updateOrderStatusToGetOrders.endpoint.test.js
@@ -28,8 +28,6 @@ beforeAll(async () => {
     mongoServer = await MongoMemoryServer.create();
 
     const uri = mongoServer.getUri("updateStatus-test-db"); // different name
-
-    await mongoose.disconnect();
     await mongoose.connect(uri);
 });
 

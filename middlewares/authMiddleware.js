be93a3c0e8f4d918ee0335d4460030379be52eaf
KimHan01 <teokimhan10@hotmail.com>
2026-04-08 18:31:27 +0800

    add limiter to login route

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,6 @@
 import JWT from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+import { rateLimit } from 'express-rate-limit'
 
 // Protected routes token base
 export const requireSignIn = async (req, res, next) => {
@@ -41,3 +42,18 @@ export const isAdmin = async (req, res, next) => {
         });
     }
 };
+
+export const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes 
+	limit: 3, // Limit each IP to 3 requests per `window` (here, per 1 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    message: { message: 'Too many requests, please try again later.' },
+    skipSuccessfulRequests: true, // Only count failed requests (invalid password) towards the rate limit
+    requestWasSuccessful: (req, res) => {
+        // only count requests that result in a 401 (invalid password) as failed attempts
+        return res.statusCode != 401; 
+    },
+    validate: {xForwardedForHeader: false},
+});
